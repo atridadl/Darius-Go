f55f5147d35e14cf8dde9cc7b0c4a012598b315a
atridadl <me@atri.dad>
2023-11-06 22:27:00 -0700

    Update header

--- pages/index.go
+++ pages/index.go
@@ -4,6 +4,7 @@ import "github.com/gofiber/fiber/v2"
 
 func IndexHandler(c *fiber.Ctx) error {
 	return c.Render("index", fiber.Map{
-		"Title": "Home",
+		"Title":       "Home",
+		"Description": "🚀 A Web Application Template Powered by HTMX + Go Fiber + Tailwind 🚀",
 	})
 }
